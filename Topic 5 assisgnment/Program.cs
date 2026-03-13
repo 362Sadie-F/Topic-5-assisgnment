8ce9affdfd57f7d86c2c1d951f00ff08e41dd479
362Sadie-F <princessmae.fleming@gmail.com>
2026-03-13 12:31:32 -0400

    Add project files.

---
+++ Topic 5 assisgnment/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Topic_5_assisgnment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
