42e2f10275eb73044b22c0e776d218aeb54f6703
362Sadie-F <princessmae.fleming@gmail.com>
2026-03-13 12:44:03 -0400

    task 1

--- Topic 5 assisgnment/Program.cs
+++ Topic 5 assisgnment/Program.cs
@@ -4,7 +4,53 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            //task 1
+            int planet;
+            double weight;
+
+            Console.Write("Enter your current weight: ");
+            weight = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("I have information on the following planets: ");
+            Console.WriteLine("1. Venus");
+            Console.WriteLine("2. Mars");
+            Console.WriteLine("3. Jupiter");
+            Console.WriteLine("4. Saturn");
+            Console.WriteLine("5. Uranus");
+            Console.WriteLine("6. Neptune");
+            Console.Write("Enter the number of the planet you'll be visiting: ");
+            planet = Convert.ToInt32(Console.ReadLine());
+            if (planet == 1)
+            {
+                Console.WriteLine("Your weight on Venus is: " + weight*0.78 + " pounds");
+            }
+            if (planet == 2)
+            {
+                Console.WriteLine("Your weight on Mars is: " + weight * 0.39 + " pounds");
+            }
+            if (planet == 3)
+            {
+                Console.WriteLine("Your weight on Jupiter is: " + weight * 2.65 + " pounds");
+            }
+            if (planet == 4)
+            {
+                Console.WriteLine("Your weight on Saturn is: " + weight * 1.17 + " pounds");
+            }
+            if (planet == 5)
+            {
+                Console.WriteLine("Your weight on Uranus is: " + weight * 1.05 + " pounds");
+            }
+            if (planet == 6)
+            {
+                Console.WriteLine("Your weight on Neptune is: " + weight * 1.23 + " pounds");
+            }
+            
+
+
+
+            //task 2
+
+           // Console.WriteLine();
         }
     }
 }
